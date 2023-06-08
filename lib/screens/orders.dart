14ef75d4330ab85ff1f3cafb47bce5bd96adbe74
bensaadat <bensaadat.amine@gmail.com>
2023-06-08 15:22:30 +0100

    slidale update

--- lib/screens/orders.dart
+++ lib/screens/orders.dart
@@ -1,5 +1,6 @@
 import 'package:buttons_tabbar/buttons_tabbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class Orders extends StatefulWidget {
   const Orders({
@@ -10,9 +11,43 @@ class Orders extends StatefulWidget {
   State<Orders> createState() => _OrdersState();
 }
 
+class DataList {
+  String name, title, date;
+  DataList({required this.name, required this.title, required this.date});
+}
+
 class _OrdersState extends State<Orders> {
+  List<DataList> items = [];
   @override
   Widget build(BuildContext context) {
+    setState(() {
+      items = List.of([
+        DataList(
+            name: 'Amine Bnesaadat',
+            title: "Audiance - Tribunal Casa B",
+            date: '05/04/2023'),
+        DataList(
+            name: 'Ahmed Abc',
+            title: "Rendez-vous - Tribunal Casa R",
+            date: '08/09/2023'),
+        DataList(
+            name: 'Rachid rachdi',
+            title: "Depalacement - Tribunal Casa Meknas",
+            date: '08/09/2023'),
+        DataList(
+            name: 'meryem afnan',
+            title: "P1 - Tribunal Fes",
+            date: '08/09/2023'),
+        DataList(
+            name: 'Amine Bnesaadat',
+            title: "Audiance - Tribunal Casa B",
+            date: '05/04/2023'),
+        DataList(
+            name: 'Ahmed Abc',
+            title: "Rendez-vous - Tribunal Casa R",
+            date: '08/09/2023'),
+      ]);
+    });
     return Container(
       margin: EdgeInsets.fromLTRB(10, 20, 10, 0),
       child: DefaultTabController(
@@ -112,6 +147,7 @@ class _OrdersState extends State<Orders> {
                                 ],
                               ),
                             )),
+                        SlidableWidget()
                       ],
                     ),
                   ),
@@ -132,3 +168,80 @@ class _OrdersState extends State<Orders> {
     );
   }
 }
+
+class SlidableWidget extends StatelessWidget {
+  const SlidableWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      // Specify a key if the Slidable is dismissible.
+      key: const ValueKey(0),
+
+      // The start action pane is the one at the left or the top side.
+      startActionPane: ActionPane(
+        // A motion is a widget used to control how the pane animates.
+        motion: const ScrollMotion(),
+
+        // A pane can dismiss the Slidable.
+        dismissible: DismissiblePane(onDismissed: () {}),
+
+        // All actions are defined in the children parameter.
+        children: const [
+          // A SlidableAction can have an icon and/or a label.
+          SlidableAction(
+            onPressed: doNothing,
+            backgroundColor: Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+          SlidableAction(
+            onPressed: doNothing,
+            backgroundColor: Color(0xFF21B7CA),
+            foregroundColor: Colors.white,
+            icon: Icons.share,
+            label: 'Share',
+          ),
+        ],
+      ),
+
+      // The end action pane is the one at the right or the bottom side.
+      endActionPane: const ActionPane(
+        motion: ScrollMotion(),
+        children: [
+          SlidableAction(
+            // An action can be bigger than the others.
+            flex: 2,
+            onPressed: doNothing,
+            backgroundColor: Color(0xFF7BC043),
+            foregroundColor: Colors.white,
+            icon: Icons.archive,
+            label: 'Archive',
+          ),
+          SlidableAction(
+            onPressed: doNothing,
+            backgroundColor: Color(0xFF0392CF),
+            foregroundColor: Colors.white,
+            icon: Icons.save,
+            label: 'Save',
+          ),
+        ],
+      ),
+
+      // The child of the Slidable is what the user sees when the
+      // component is not dragged.
+      child: Container(
+        child: Row(
+          children: [
+            Text('vcvcfv'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void doNothing(BuildContext context) {}
